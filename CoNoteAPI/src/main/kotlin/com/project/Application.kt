f7af13d3d6da1aea1351b8446f81a0e916fd46d2
gleb <gleb.nikishin.2003@gmail.com>
2024-12-13 21:16:07 +0300

    initial commit

---
+++ CoNoteAPI/src/main/kotlin/com/project/Application.kt
@@ -0,0 +1,13 @@
+package com.project
+
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+
+fun main() {
+    embeddedServer(Netty, port = 8080, module = Application::module).start(wait = true)
+}
+
+fun Application.module() {
+    configureRouting()
+}
